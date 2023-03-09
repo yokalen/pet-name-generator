48805dff12fb8412d579528d6b77eb787efb63ca
My Computer <yokalen@gmail.com>
2023-03-08 20:07:19 -0800

    import style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
 
 export default function Home() {
     const [animalInput, setAnimalInput] = useState('');
@@ -15,41 +17,41 @@ export default function Home() {
                     body: JSON.stringify({ animal: animalInput }),
                 });
             
-            const data = await response.json();
-            if (response.status !== 200) {
-                throw data.error || new Error('Request failed with status code ${response.status}');
+                const data = await response.json();
+                if (response.status !== 200) {
+                    throw data.error || new Error(`Request failed with status code ${response.status}`);
+                }
+
+                setResult(data.result);
+                setAnimalInput('');
+            }catch(error){
+                console.error(error);
+                alert(error.message);
             }
+        }
 
-            setResult(data.result);
-            setAnimalInput('');
-        }catch(error){
-            console.error(error);
-            alert(error.message);
-    }
-}
+        return (
+            <div>
+                <Head>
+                    <title>Pet Name Generator</title>
+                    <link rel="icon" href="/dog.png" />
+                </Head>
 
-    return (
-        <div>
-            <Head>
-                <title>Pet Name Generator</title>
-                <link rel="icon" href="/dog.png" />
-            </Head>
-
-            <main className={styles.main}>
-                <img src="/dog.png" className={styles.icon} />
-                <h3>Name my pet</h3>
-                <form onSubmit={onSubmit}>
-                    <input
-                        type="text"
-                        name="animal"
-                        placeholder="Enter an animal"
-                        value={animalInput}
-                        onChange={(event) => setAnimalInput(event.target.value)}
-                    />
-                    <input type="submit" value="Generate names" />
-                </form>
-                <div className={styles.result}>{result}</div>
-            </main>
-        </div>
+                <main className={styles.main}>
+                    <img src="/dog.png" className={styles.icon} />
+                    <h3>Name my pet</h3>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            name="animal"
+                            placeholder="Enter an animal"
+                            value={animalInput}
+                            onChange={(event) => setAnimalInput(event.target.value)}
+                        />
+                        <input type="submit" value="Generate names" />
+                    </form>
+                    <div className={styles.result}>{result}</div>
+                </main>
+            </div>
         );
 }
